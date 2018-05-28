1fe1334f027863a2bdf9db08ccdd5d49b2ad6d11
Victoria Daug <victoria.daug@gmail.com>
2018-05-28 17:36:44 +0100

    elements schema

---
+++ models/elements.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const elementSchema = new mongoose.Schema({
+  name: { type: String },
+  symbol: { type: String },
+  atomic weight: { type: Number },
+  number: { type: Number }
+});
+
+elementSchema.set('toObject', {
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
+module.exports = mongoose.model('Element', elementSchema);
