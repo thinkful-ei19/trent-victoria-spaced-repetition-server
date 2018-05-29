66756ac3e6a1ec7afcec997878cb96af6cf5ae25
trentduncan <trent.dunc@gmail.com>
2018-05-29 11:34:54 -0700

    added pseudocode for put endpoint

--- routes/questions.js
+++ routes/questions.js
@@ -16,7 +16,30 @@ router.get('/questions', jwtAuth, (req,res, next) => {
   const {username} = req.user; 
   return User.find({username})
     .then(([result]) => {
+    // as of now the answer is sent to the client, down the road make 
+    // it not send the answer
       res.json(result.questions[0]);
+    })
+    .catch(err => next(err));
+});
+
+router.put('/questions', (req,res, next) => {
+  const {username} = req.user; 
+  const userGuess = req.body.name.toLowerCase();
+  let answer;
+  let isCorrect;
+  return User.find({username})
+    .then(([result]) => {
+      // this first part pulls the current question so it can grab the answer
+      answer = result.questions[0].name.toLowerCase();
+      if (answer === userGuess) {
+        isCorrect = true;
+        let newQuestions = [...result.questions.slice(1), result.questions[0]];
+        // return mongoose updating the questions with the new questions and sending the new top question to client
+      } else {
+        // is correct to true and return a promise changing the questions order to reflect the incorrect answer
+        //  and send the new next question to the client
+      }
     })
     .catch(err => next(err));
 });
