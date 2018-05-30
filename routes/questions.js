67126e9008454e0e06de4e9f59e24ba768434233
trentduncan <trent.dunc@gmail.com>
2018-05-30 13:49:00 -0700

    changed points to reflect current point valueafter getting a question right

--- routes/questions.js
+++ routes/questions.js
@@ -43,29 +43,8 @@ router.get('/questions', jwtAuth, (req,res, next) => {
     .catch(err => next(err));
 });
 
-// Intermediate Algorithm
-// router.put('/questions', (req,res, next) => {
-//   const {username} = req.user; 
-//   const userGuess = req.body.name.toLowerCase();
-//   let answer;
-//   let isCorrect;
-//   return User.find({username})
-//     .then(([result]) => {
-//       // this first part pulls the current question so it can grab the answer
-//       answer = result.questions.head.value.name.toLowerCase();
-//       if (answer === userGuess) {
-//         isCorrect = true;
-//         let newQuestions = [...result.questions.slice(1), result.questions[0]];
-//         // return mongoose updating the questions with the new questions and sending the new top question to client
-//       } else {
-//         // is correct to true and return a promise changing the questions order to reflect the incorrect answer
-//         //  and send the new next question to the client
-//       }
-//     })
-//     .catch(err => next(err));
-// });
 
-// Simple Algorithm
+// m-value Algorithm
 router.put('/questions', jwtAuth, (req,res, next) => {
   const {username} = req.user;
   const userGuess = req.body.name.toLowerCase().trim();
@@ -81,9 +60,10 @@ router.put('/questions', jwtAuth, (req,res, next) => {
       answer = currentNode.name.toLowerCase();
       if (answer === userGuess) {
         isCorrect = true;
+        points += 10;
         return User.updateOne({username, 'questions.next' : null}, {$set: { 'questions.$.next': head}})
           .then(() => {
-            return User.updateOne({username, 'questions.next' : currentNode.next}, {$set: { points: points + 10, head: currentNode.next, 'questions.$.next': null}});
+            return User.updateOne({username, 'questions.next' : currentNode.next}, {$set: { points: points, head: currentNode.next, 'questions.$.next': null}});
           });
       } else {
         isCorrect = false;
