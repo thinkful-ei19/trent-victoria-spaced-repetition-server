559b7207f4335d5b19a074fb831a8d339daa85b5
trentduncan <trent.dunc@gmail.com>
2018-06-01 07:31:45 -0700

    removed traces of linkedlist class, and added reserve questions to seed db

--- routes/questions.js
+++ routes/questions.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-const {LinkedList} = require('../linked-list');
 
 
 const {User} = require('../models/user');
--- utils/seed-database.js
+++ utils/seed-database.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 
 const { DATABASE_URL } = require('../config');
 const {User} = require('../models/user');
+const Question = require('../models/question');
 
 const seedUsers = require('../db/seed/users');
+const seedReserveQuestions = require('../db/seed/reserveQuestions');
 
 // seed production db 'mongodb://dev:dev@ds237700.mlab.com:37700/spaced-repetition-trent-victoria'
 mongoose.connect(DATABASE_URL)
@@ -13,7 +15,8 @@ mongoose.connect(DATABASE_URL)
   .then(() => {
     return Promise.all([
       User.insertMany(seedUsers),
-      User.createIndexes()
+      User.createIndexes(),
+      Question.insertMany(seedReserveQuestions)
     ]);
   })
   .then(() => mongoose.disconnect())
