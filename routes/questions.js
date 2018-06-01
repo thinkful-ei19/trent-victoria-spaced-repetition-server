02af99e104ba77a36387a4e05a9137b9a7dce970
trentduncan <trent.dunc@gmail.com>
2018-06-01 09:33:09 -0700

    Made each correct response start a chain of how many correct, and made new endpoint to grab a new element and place it at the end of the list

--- routes/questions.js
+++ routes/questions.js
@@ -6,6 +6,8 @@ const jwt = require('jsonwebtoken');
 
 
 const {User} = require('../models/user');
+const Question = require('../models/question');
+
 const { JWT_SECRET, JWT_EXPIRY } = require('../config');
 const router = express.Router();
 
@@ -33,10 +35,12 @@ router.put('/questions', jwtAuth, (req,res, next) => {
   let points;
   let correct;
   let incorrect;
+  let isCardAdded = false;
   return User.find({username})
     .then(([result]) => {
       const head = result.head;
       points = result.points;
+      let inARow = result.inARow;
       let headNode = result.questions[head];
       let currentNode = result.questions[head];
       let mValue = headNode.m;
@@ -48,18 +52,24 @@ router.put('/questions', jwtAuth, (req,res, next) => {
         points += 10;
         mValue = mValue*2;
         correct += 1;
+        inARow += 1;
       } else {
         isCorrect = false;
         mValue = 1;
         incorrect += 1;
+        inARow = 0;
+      }
+      if (inARow === 10) {
+        inARow = 0;
+        isCardAdded = true;
       }
       if (mValue > result.questions.length - 1) {
         return User.updateOne({username, 'questions.next' : null}, {$set: { 'questions.$.next': head}})
           .then(() => {
             if (isCorrect) {
-              return User.updateOne({username, 'questions.next' : headNode.next}, {$set: { points: points, head: headNode.next, 'questions.$.next': null,'questions.$.m' : mValue, 'questions.$.correct' : correct}});
+              return User.updateOne({username, 'questions.next' : headNode.next}, {$set: { inARow: inARow, points: points, head: headNode.next, 'questions.$.next': null,'questions.$.m' : mValue, 'questions.$.correct' : correct}});
             } else {
-              return User.updateOne({username, 'questions.next' : headNode.next}, {$set: { points: points, head: headNode.next, 'questions.$.next': null,'questions.$.m' : mValue, 'questions.$.incorrect' : incorrect}});
+              return User.updateOne({username, 'questions.next' : headNode.next}, {$set: { inARow: inARow, points: points, head: headNode.next, 'questions.$.next': null,'questions.$.m' : mValue, 'questions.$.incorrect' : incorrect}});
             }
             
           });
@@ -67,7 +77,7 @@ router.put('/questions', jwtAuth, (req,res, next) => {
       for (let i = 0; i < mValue; i++) {
         currentNode = result.questions[currentNode.next];
       }
-      return User.updateOne({username, 'questions.next' : currentNode.next}, {$set: { points: points, head: headNode.next, 'questions.$.next': head}})
+      return User.updateOne({username, 'questions.next' : currentNode.next}, {$set: { inARow: inARow, points: points, head: headNode.next, 'questions.$.next': head}})
         .then(() => {
           if (isCorrect) {
             return User.updateOne({username, 'questions.next' : headNode.next}, {$set: { 'questions.$.next': currentNode.next, 'questions.$.m' : mValue, 'questions.$.correct' : correct}});
@@ -77,7 +87,46 @@ router.put('/questions', jwtAuth, (req,res, next) => {
         });
     }) 
     .then(() => {
-      return res.json({answer, isCorrect, points, correct}); 
+      return res.json({answer, isCorrect, points, correct, isCardAdded}); 
+    })
+    .catch(err => next(err));
+});
+
+function convertNewQuestionObject(obj) {
+  const {name, symbol, atomicWeight, number,next,m,correct, incorrect} = obj;
+  return {
+    name, 
+    symbol, 
+    atomicWeight, 
+    number,
+    next,
+    m,
+    correct, 
+    incorrect
+  };
+}
+
+router.put('/reserve', jwtAuth, (req,res, next) => {
+  const {username} = req.user;
+  let nextQuestion;
+  return User.find({username})
+    .then(([result]) => {
+      let {nextReserve} = result; 
+      let newLastQuestion = result.questions.length;
+      return Question.find()
+        .then(result => {
+          nextQuestion = convertNewQuestionObject(result[nextReserve]);
+        })
+        .then(() => {
+          nextReserve += 1;
+          return User.updateOne({username, 'questions.next' : null}, {$set: { nextReserve: nextReserve, 'questions.$.next': newLastQuestion}});
+        })
+        .then(() => {
+          return User.updateOne({username}, { $push: { questions: nextQuestion } });
+        });
+    })
+    .then(() => {
+      return res.json(nextQuestion); 
     })
     .catch(err => next(err));
 });
